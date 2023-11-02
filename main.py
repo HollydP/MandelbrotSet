750e879131cd9f093587a2c5935570ad450e8bef
Nina van der Meulen <ninaajwzz@gmail.com>
2023-11-02 16:29:11 +0100

    comment added

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 # Contains all code to generate the images in the images folder, used in the report.
 # Usage: python3 main.py
+# Maybe we can add CLA's to run random/hypercube/orthogonal sampling etc.? 
