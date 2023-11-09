4b1543337f52e326826dc95f30b102ac6a465c6c
HollydP <hollydups@gmail.com>
2023-11-09 20:45:26 +0100

    add randomization to final point

--- libraries/orthogonal_sampling.py
+++ libraries/orthogonal_sampling.py
@@ -30,34 +30,52 @@ class OrthogonalSampling:
     def create_samples(self) -> ndarray[complex]:
         """
         Generate a list of complex numbers via orthogonal sampling.
-        """
-        dx = (self.x_max-self.x_min)/(self.samples-1)
-        dy = (self.y_max-self.y_min)/(self.samples-1)
 
+        - Seperate lattice into a number of subgrids.
+        - In each subgrid randomly select a cell.
+        - Make sure no cells share a common row or column with the cells selected in other subgrids.
+        - Randomly select location within cell
+        """
+        # Size of cell
+        dx = (self.x_max-self.x_min)/(self.samples-1) # width
+        dy = (self.y_max-self.y_min)/(self.samples-1) # height
+
+        # Create subgrids by index
         subspaces = int(np.sqrt(self.samples))
         blocks = {(i,j):[(a,b) for a in range(i*subspaces,i*subspaces+subspaces) for b in range(j*subspaces,j*subspaces+subspaces)] 
                 for i in range(subspaces) for j in range(subspaces)}
 
+        # Initilize arrays to keep track of which rows and cells have been selected
         selected_row=[]
         selected_col=[]
+
+        # Loop through each subgrid so all subgrids have 1 sample
         for i in range(subspaces):
             for j in range(subspaces):
                 # print(i,j)
                 n=m=np.nan
                 match = False
 
+                # Randomly select a cell within subgrid
                 while match == False:
                     (n,m) = random.sample(blocks[(i,j)], k=1)[0]
+                    # check row and column of cell is different from previously selected cells
                     if n not in selected_row:
                         if m not in selected_col:
-                            print(n,m)
                             selected_row.append(n)
                             selected_col.append(m)
                             match = True
 
-        selected_row = self.x_min + np.array(selected_row)*dx
-        selected_col = self.y_min +  np.array(selected_col)*dy
-        paired_points = list(zip(selected_row,selected_col))
+        # Convert index to location on the plane
+        # each point describes the centre of a cell
+        x_pos = self.x_min + np.array(selected_row)*dx
+        y_pos = self.y_min +  np.array(selected_col)*dy
+
+        # randomize to a point within cell
+        random_x_pos =  x_pos + np.random.uniform(-1,1,subspaces)*dx/2
+        random_y_pos = y_pos  + np.random.uniform(-1,1,subspaces)*dy/2
+
+        paired_points = list(zip(random_x_pos,random_y_pos))
 
         complex_numbers = np.array([complex(a,b) for a, b in paired_points])
 
